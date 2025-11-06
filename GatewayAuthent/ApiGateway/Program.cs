4841575acf4522a8185621b68fd96ce692e99482
sarusman <sarusmandu92@gmail.com>
2025-11-06 22:15:29 +0100

    fd

--- GatewayAuthent/ApiGateway/Program.cs
+++ GatewayAuthent/ApiGateway/Program.cs
@@ -1,42 +1,9 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Yarp.ReverseProxy;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(o =>
-    {
-        var authority = builder.Configuration["Jwt:Authority"]!;
-        var audiences = builder.Configuration.GetSection("Jwt:Audiences").Get<string[]>() ?? Array.Empty<string>();
-        o.Authority = authority;
-        o.RequireHttpsMetadata = false;
-        o.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = authority,
-            ValidateAudience = true,
-            ValidAudiences = audiences,
-            ValidateLifetime = true,
-            NameClaimType = "preferred_username",
-            RoleClaimType = "roles"
-        };
-    });
-
-builder.Services.AddAuthorization();
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowBlazorClient", policy =>
-        policy.WithOrigins("http://localhost:5165")
-              .AllowAnyHeader()
-              .AllowAnyMethod());
-});
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapReverseProxy();
 
